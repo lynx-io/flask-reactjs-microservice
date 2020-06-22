0819602a65d86fc40c827ed1893785b8dc486ce5
JESUS  COPADO ESPEJO <1720212@chester.ac.uk>
2020-06-22 19:12:06 +0200

    changed dockercompose version in travis.yml

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -3,28 +3,27 @@ import os
 
 class BaseConfig:
     """Base Configuration"""
-
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "my_precious"
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development config"""
-
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DEBUG_TB_ENABLED = True
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
-
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
 
 
 class ProductionConfig(BaseConfig):
     """Production configuratio"""
-
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     pass
