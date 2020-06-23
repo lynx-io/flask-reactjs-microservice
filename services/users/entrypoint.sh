019280200df77d227ed5d1a07737fbd47ff2d105
Jesus Copado Espejo <jsscpdspj94@gmail.com>
2020-06-23 13:00:17 +0200

    ReactJS added

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -8,5 +8,8 @@ done
 
 echo "PostgreSQL started"
 
+python manage.py recreate-db
+python manage.py seed-db
 python manage.py run -h 0.0.0.0
+
 
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 import os
 
 
@@ -12,6 +13,9 @@ toolbar = DebugToolbarExtension()
 def create_app(script_info=None):
     # instantiate the app
     app = Flask(__name__)
+
+    # Enable CORS
+    CORS(app)
 
     # set config
     app_settings = os.getenv("APP_SETTINGS")
