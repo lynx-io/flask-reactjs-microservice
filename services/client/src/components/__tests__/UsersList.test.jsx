89e676018dff114ba60b645723066e4b22201363
Jesus Copado Espejo <jsscpdspj94@gmail.com>
2020-06-23 13:16:53 +0200

    ReactJS test added

---
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import UsersList from '../UsersList';
+import renderer from 'react-test-renderer';
+
+const users = [
+    {
+        'active': true,
+        'email': 'jesus@gmail.com',
+        'id': 1,
+        'username': 'jesus'
+    },
+    {
+        'active': true,
+        'email': 'jesus1@gmail.com',
+        'id': 2,
+        'username': 'jesus1'
+    }
+];
+
+test('UsersList renders properly', () => {
+    const wrapper = shallow(<UsersList users={users} />);
+    const element = wrapper.find('h4');
+    expect(element.length).toBe(2);
+    expect(element.get(0).props.className).toBe('card card-body bg-light');
+    expect(element.get(0).props.children).toBe('jesus');
+});
+
+test('UsersList renders a snapshot properly', () => {
+    const tree = renderer.create(<UsersList users={users} />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
