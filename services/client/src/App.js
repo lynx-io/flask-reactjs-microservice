79611b2ec866d02bb54a31a60dfff8363d99ecdf
Jesus Copado Espejo <jsscpdspj94@gmail.com>
2020-06-27 11:12:02 +0100

    Added Forms

--- services/client/src/App.js
+++ services/client/src/App.js
@@ -5,6 +5,7 @@ import AddUser from './components/AddUser';
 import About from './components/About';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Form from './components/Form'
 
 class App extends Component {
   constructor() {
@@ -13,7 +14,12 @@ class App extends Component {
       users: [],
       username: '',
       email: '',
-      title: 'TestDriven.io'
+      title: 'TestDriven.io',
+      formData: {
+        username: '',
+        email: '',
+        password: ''
+      }
     };
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -55,21 +61,18 @@ class App extends Component {
             <div className="col-md-6">
               <br />
               <Switch>
-                <Route exact path='/' render={() => (
-                  <div>
-                    <h1>All Users</h1>
-                    <hr /><br />
-                    <AddUser
-                      username={this.state.username}
-                      email={this.state.email}
-                      handleChange={this.handleChange}
-                      addUser={this.addUser}
-                    />
-                    <br />
-                    <UsersList users={this.state.users} />
-                  </div>
+                <Route exact path='/register' render={() => (
+                  <Form
+                    formType={'Register'}
+                    formData={this.state.formData}
+                  />
                 )} />
-                <Route exact path='/about' component={About} />
+                <Route exact path='/login' render={() => (
+                  <Form
+                    formType={'Login'}
+                    formData={this.state.formData}
+                  />
+                )} />
               </Switch>
             </div>
           </div>
